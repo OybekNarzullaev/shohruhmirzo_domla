339c13551b94588bc27a0415cabcc20d7b6c362d
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2025-10-28 15:39:21 +0500

    test

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'apps', 'client', 'dist'),
+    os.path.join(BASE_DIR, 'apps', 'client', 'dist', 'static'),
 ]
 
 STATIC_ROOT = BASE_DIR / 'static_files'
